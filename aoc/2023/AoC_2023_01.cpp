c61ed96078db167c37cff2a32b7a9813d15b6143
Lorenzo <lorenzombolis@gmail.com>
2024-06-21 17:10:48 +0200

    Add 2023 day 1

---
+++ aoc/2023/AoC_2023_01.cpp
@@ -0,0 +1,3 @@
+#include "AoC_2021_01.hpp"
+
+// This file is intentionally left blank.
---
+++ aoc/2023/AoC_2023_01.hpp
@@ -0,0 +1,41 @@
+#ifndef AOC_2023_01_HEADER_GUARD
+#define AOC_2023_01_HEADER_GUARD
+///////////////////////////////////////////////////////////////////////////////
+// File AoC_2023_01.hpp
+///////////////////////////////////////////////////////////////////////////////
+#include <cstddef> // size_t
+
+namespace aoc {
+
+// Return the number of elements in 'measurements' which are larger than the
+// previous element. Note that if 'measurements' is empty, the result is 0.
+// 'Range' must model a range whose value type can be ordered.
+template <typename Range>
+std::size_t count_increases(Range const& measurements) noexcept;
+
+} // namespace aoc
+
+///////////////////////////////////////////////////////////////////////////////
+// Template definitions
+///////////////////////////////////////////////////////////////////////////////
+#include <iterator> // next
+
+template <typename Range>
+std::size_t aoc::count_increases(Range const& measurements) noexcept {
+  if (cbegin(measurements) == cend(measurements)) {
+    return 0;                                                         // RETURN
+  }
+  // Note that we keep track of two iterators, which implies we are using
+  // forward iterators; extending to input iterators would require storing a
+  // copy of the read element.
+  // TODO: implement an iterator which zips adjacent elements.
+  std::size_t count = 0;
+  for (auto prev = cbegin(measurements), cur = std::next(prev);
+       cur != cend(measurements);
+       prev = cur++) { // adjacent_zip | count_if(<)
+    if (*prev < *cur) ++count;
+  }
+  return count;
+}
+
+#endif // AOC_2023_01_HEADER_GUARD
